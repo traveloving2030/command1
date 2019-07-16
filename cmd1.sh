30c2782f58d8a9d491e15d2092bfc986f1ce7552
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-07-16 18:29:37 +0900

    Update cmd1.sh

--- cmd1.sh
+++ cmd1.sh
@@ -1,23 +1,3 @@
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-printf "\n"
-printf "node install complete\n"
-
-docker pull ptunstad/fabric-baseos:arm64-0.4.15 &&
-docker pull ptunstad/fabric-basejvm:arm64-0.4.15 &&
-docker pull ptunstad/fabric-baseimage:arm64-0.4.15 &&
-docker pull ptunstad/fabric-ccenv:arm64-1.4.1 &&
-docker pull ptunstad/fabric-peer:arm64-1.4.1 &&
-docker pull ptunstad/fabric-orderer:arm64-1.4.1 &&
-docker pull ptunstad/fabric-zookeeper:arm64-1.4.1 &&
-docker pull ptunstad/fabric-kafka:arm64-1.4.1 &&
-docker pull ptunstad/fabric-couchdb:arm64-1.4.1 &&
-docker pull ptunstad/fabric-tools:arm64-1.4.1
-
-printf "\n"
-printf "docker pull complete\n"
-
 sudo mkdir /data && sudo chmod -R ugo+rw /data
 git clone -b "rpi" https://github.com/Tunstad/Hyperprov.git
 
