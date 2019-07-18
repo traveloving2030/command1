a7dc57435ac96e27a36d3b3430d533512d1cd1fb
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-07-18 17:01:12 +0900

    Update cmd0.sh

--- cmd0.sh
+++ cmd0.sh
@@ -47,17 +47,5 @@ docker pull ptunstad/fabric-kafka:arm64-1.4.1 &&
 docker pull ptunstad/fabric-couchdb:arm64-1.4.1 &&
 docker pull ptunstad/fabric-tools:arm64-1.4.1
 
-sudo mkdir /data && sudo chmod -R ugo+rw /data
-git clone -b "rpi" https://github.com/Tunstad/Hyperprov.git
-
-sudo fallocate -l 1G /swapfile
-sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
-
-printf "\n"
-printf "docker swarm complete\n"
 
 
