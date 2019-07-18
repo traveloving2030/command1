12c51b2a641a6d39d58fb83ad989f29a04f933bf
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-07-18 20:36:18 +0900

    Update cmd2.sh

--- cmd2.sh
+++ cmd2.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-4p1f6mkr3mygplepxwwt610crhcuhcc56knldxzq5ooeng9cl-b7hmlwcbimor44hqtm955g8mr 192.168.9.116:2377
+docker swarm join --token SWMTKN-1-4p1f6mkr3mygplepxwwt610crhcuhcc56knldxzq5ooeng9kcl-b7hmlwcbimor44hqtm955g8mr 192.168.9.116:2377
