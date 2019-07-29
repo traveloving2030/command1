32427bd316815efa4f6928c937eed9ae7f41f67c
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-07-29 22:48:35 +0900

    Update cmd2.sh

--- cmd2.sh
+++ cmd2.sh
@@ -1,6 +1,1 @@
-rm -rf command1
-git clone https://github.com/traveloving2030/command1
-cd command1
-sudo chmod +x cmd2.sh
-sudo ./cmd2.sh
 docker swarm join --token SWMTKN-1-3o1djw68eo2ku7gu5ns83zxgakd2dab2oam2v457p25o8250vu-croucdq1lxltkh37tcsfv5c85 192.168.9.107:2377
