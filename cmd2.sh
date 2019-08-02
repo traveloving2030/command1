562b525831fe9e23824dbc5292ab13fd0fec1f4c
KimYC1223 <40852277+KimYC1223@users.noreply.github.com>
2019-08-03 00:00:06 +0900

    Update cmd2.sh

--- cmd2.sh
+++ cmd2.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-4awmd1lx00m41jkr7cfwo9p211dorlr4py26y0k35df0r8z224-3d3ehf9xyokfrbojgxpzum2sf 192.168.9.107:2377
+docker swarm join --token SWMTKN-1-0f8hnfo1vjkvavm6z0klqmjzv2p5yk1warm3rrdh50nxdnlcr9-cwqsp4pm9qqfuapwd7acs74wo 192.168.9.107:2377
