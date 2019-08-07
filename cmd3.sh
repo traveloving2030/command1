e8d798796e9f8dcbba8e617639788c9814fb3bc9
traveloving2030 <45926066+traveloving2030@users.noreply.github.com>
2019-08-07 21:22:59 +0900

    Create cmd3.sh

---
+++ cmd3.sh
@@ -0,0 +1,11 @@
+sudo docker pull hyperledger/fabric-baseos:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-basejvm:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-baseimage:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-ccenv:amd64-1.4.1 &&
+sudo docker pull hyperledger/fabric-peer:amd64-1.4.1 &&
+sudo docker pull hyperledger/fabric-orderer:amd64-1.4.1 &&
+sudo docker pull hyperledger/fabric-zookeeper:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-kafka:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-couchdb:amd64-0.4.15 &&
+sudo docker pull hyperledger/fabric-tools:amd64-1.4.1 &&
+sudo docker pull hyperledger/fabric-javaenv:amd64-1.4.1
